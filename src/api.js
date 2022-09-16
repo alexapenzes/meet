1ea98b1fd48a47c3c3109d7917fd2246931859c0
alexapenzes <penzesalexa@gmail.com>
2022-09-16 21:00:15 +0200

    Added 'extractLocations' to API

--- src/api.js
+++ src/api.js
@@ -11,6 +11,12 @@ import { mockData } from "./mockData";
 import axios from "axios";
 import NProgress from 'nprogress';
 
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+};
+
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem("access_token");
   const tokenCheck = accessToken && (await checkToken(accessToken));
